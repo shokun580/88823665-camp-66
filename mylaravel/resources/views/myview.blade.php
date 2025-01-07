a0dbebd441718ed1ce1494ce5323828772776def
Aphinan <154401387+shokun580@users.noreply.github.com>
2025-01-07 19:46:57 +0700

    first_try

---
+++ mylaravel/resources/views/myview.blade.php
@@ -0,0 +1,17 @@
+<h1>My view</h1>
+<?php echo $value_id;?>
+{{$value_id;}}{{$myinput}}
+<form action="{{url('/mycontroller')}}" method="POST">
+    @csrf
+    <input type = "text" name = "myinput">
+    <button type = "sumbit">
+        submit
+    </button>
+
+</form>
+<?php for($i=1;$i<13;$i++){?>
+    <?php
+    $ans = intval($value_id) * $i;
+    ?>
+    <h1>{{$ans}}</h1>
+<?php }?>
--- mylaravel/routes/web.php
+++ mylaravel/routes/web.php
@@ -1,10 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MyController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/hello', function () {
     return "</h1>hello world</h1>";
 });
+Route::get("/mycontroller",
+[MyController::class,'myfunction']);
+
+
+Route::get("/mycontroller/{id?}",
+[MyController::class,'myfunction']);
+
+Route::post('/mycontroller/{id?}',
+[MyController::class,'myfunction']);
